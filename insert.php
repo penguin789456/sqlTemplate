758a95445ff61216617d386952d842ea84fad3bb
penguin123456789 <107233946+penguin789456@users.noreply.github.com>
2023-07-26 16:30:58 +0800

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+    $id=$_POST["ID"];
+    $attribute=$_POST["Attribute"];
+    $price=$_POST["Price"];
+    $time=$_POST["time"];
+    $link=mysqli_connect( 'localhost', // MySQL主機名稱
+    'root', // 使用者名稱 
+    '11010112', // 密碼 
+    'supermarket');
+    $sql = "INSERT INTO product values('".$id."','".$attribute."','".$price."','".$time."')";
+    echo $sql;
+    if ( $result = mysqli_query($link, $sql) ) { 
+        echo "建立成功";
+    }
+    mysqli_close($link);
+?>
